9759923f8057113d0acd014cce69705e20353bcc
Sarah Pontes <sarahspontes@gmail.com>
2021-11-12 12:22:57 -0300

    Operadores Básicos e Relacionais no Software R

---
+++ Operações - Básicos e Relacionais (2).R
@@ -0,0 +1,57 @@
+# Operações em R
+
+# Soma
+7 + 7
+
+# Subtração
+7 - 4
+
+# Multiplicação
+5 * 5
+
+# Divisão
+6 / 6
+
+# Potência
+3^2
+3**2
+
+# Módulo é o restante da divisão
+16 %% 3 
+
+
+# Operadores Relacionais
+
+# Atribuindo variáveis
+x = 7
+y = 5
+
+# Operadores
+x > 8
+x < 8
+x <= 8
+x >= 8
+x == 8
+
+#Negação
+x != 8 
+
+
+# Operadores Lógicos
+
+# And = E
+(x==8) & (x==6)
+(x==7) & (x>=5)
+(x==8) & (x==7)
+
+# Or = OU
+(x==8) | (x>5)
+(x==8) | (x>=5)
+
+# Not = FALSO
+x > 8
+print(!x > 8)
+
+
+
+
